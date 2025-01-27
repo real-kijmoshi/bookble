7e10338f398e00b0c33339147cca3a5eea8fdb38
igor rypniewski <94071547+real-kijmoshi@users.noreply.github.com>
2025-01-27 17:29:41 +0100

    fix v4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tawilwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tawilwindcss()],
   resolve: {
     alias: {
-      "@": "/src",
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
